be9fa6110c99c2726041bc4e9f41baae80e6466e
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-05-27 21:39:27 +0600

    complete all basic functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
 
-document.getElementById('login-btn').addEventListener('click', ()=> {
-    var loginArea = document.querySelector('.login-area');
-    var transactionArea = document.querySelector('.transaction-area');
+
+// login button event
+    const loginBtn = document.getElementById('login-btn');
+    loginBtn.addEventListener('click', ()=> {
+    const loginArea = document.querySelector('.login-area');
+    const transactionArea = document.querySelector('.transaction-area');
 
     loginArea.style.display = 'none';
     transactionArea.style.display = 'block';
-})
+});
+
+
+// deposite button event
+    const depositeBtn = document.getElementById('deposite-btn');
+    depositeBtn.addEventListener('click', ()=> {
+    const depositeAmount = document.getElementById('deposite-amount').value;
+    const depositeNumber = parseFloat(depositeAmount);
+
+    updateSpanText('current-deposite', depositeNumber)
+    updateSpanText('current-balance', depositeNumber);
+    document.getElementById('deposite-amount').value = '';
+
+    });
+     
+// withdraw button event
+    const withdrawBtn = document.getElementById('withdraw-btn');
+    withdrawBtn.addEventListener('click', ()=> {
+    const withdrawAmount = document.getElementById('withdraw-amount').value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+
+    updateSpanText('current-withdraw', withdrawNumber)
+    updateSpanText('current-balance', -1 * withdrawNumber);
+    document.getElementById('withdraw-amount').value = '';
+
+    });
+
+
+    function updateSpanText(id, amount) {
+        const current = document.getElementById(id).innerText;
+        const currentNumber = parseFloat(current);
+        const totalAmount = amount + currentNumber;
+        document.getElementById(id).innerText = totalAmount;
+    }
+
+ 
